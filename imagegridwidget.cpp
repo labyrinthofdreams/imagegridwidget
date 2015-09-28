1210f58defa99d2037e5333ba637fff306e5a35d
labyrinthofdreams <noemail@localhost.localhost>
2015-09-28 15:00:32 +0300

    Add license

--- imagegridwidget.cpp
+++ imagegridwidget.cpp
@@ -1,3 +1,27 @@
+/******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2015 https://github.com/labyrinthofdreams
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+******************************************************************************/
+
 #include <algorithm>
 #include <iterator>
 #include <QtCore>
@@ -86,9 +110,10 @@ Side getSide(const QPoint &needle, const QPoint &haystack) {
 
 } // namespace
 
-// TODO: See if you can combine the image edge detection in
-// dropevent and paintevent into one function
-// so that we'd get 100% equal detection in both functions
+// TODO: Implement undo
+// TODO: Implement changing spacing
+// TODO: Drag n' drop existing images in the layout
+// so that we don't have to undo the whole thing to reset
 
 ImageGridWidget::ImageGridWidget(QWidget *parent) :
     QWidget(parent),
